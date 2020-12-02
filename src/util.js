b482fb1eb4b8079ecb3282a761f6b7a23dc99fdb
jaren <jarensmart@yahoo.com>
2020-12-02 00:54:05 -0500

    Added music data

---
+++ src/util.js
@@ -0,0 +1,79 @@
+import { v4 as uuidv4 } from "uuid";
+function music() {
+  return [
+    {
+      name: "Beaver Creek",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      artist: "Aso, Middle School, Aviino",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10075",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Daylight",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+      artist: "Aiguille",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9272",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Keep Going",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+      artist: "Swørn",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9222",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Nightfall",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+      artist: "Aiguille",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9148",
+      color: ["#EF8EA9", "#ab417f"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Reflection",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+      artist: "Swørn",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9228",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Under the City Stars",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      artist: "Aso, Middle School, Aviino",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "In All Honesty, I Do",
+      cover:
+        "https://images-na.ssl-images-amazon.com/images/I/61BFVHnwU6L._AC_UL600_SR600,600_.jpg",
+      artist: "Nocturnal Spirits",
+      audio:
+        "https://r1---sn-8xgp1vo-p5qs.googlevideo.com/videoplayback?expire=1606909724&ei=vCrHX7XBH4jPhgbzlaSIAQ&ip=2600%3A4040%3A110f%3Ab600%3Aa0a1%3Ad9d5%3A921d%3A59e0&id=o-AIBR7_DIw05Lt6yA2-_QsEQZ32cpjtKvdS33EPlKp0V1&itag=18&source=youtube&requiressl=yes&mh=wc&mm=31%2C29&mn=sn-8xgp1vo-p5qs%2Csn-p5qs7n7d&ms=au%2Crdu&mv=m&mvi=1&pcm2cms=yes&pl=36&gcr=us&initcwndbps=1801250&vprv=1&mime=video%2Fmp4&ns=ESTfp8oI2fy95lFj2nxRPDYF&gir=yes&clen=5843071&ratebypass=yes&dur=334.111&lmt=1553208661916948&mt=1606887681&fvip=1&c=MWEB&txp=2311222&n=cIfuWpIiNp6aog&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cgcr%2Cvprv%2Cmime%2Cns%2Cgir%2Cclen%2Cratebypass%2Cdur%2Clmt&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpcm2cms%2Cpl%2Cinitcwndbps&lsig=AG3C_xAwRAIgCu5FckfdzK67O6OfLQPmxf49a38bDKGNlrk_NcssICMCIEqxgW06g-ODX0c_856wBO_gU21Cp1YAEStgcTrmZttq&sig=AOq0QJ8wRQIgdjHS1nJWQSWCPGGfQ9DCP3hQtyg1jMe_91rGHd4yAUoCIQC1VhqRyNSCMK6KAY5FYPn-6OFo-S1bNhb1UTkK3_VNKg%3D%3D&cpn=OcpDt73lrKqD1cek&cver=2.20201201.05.01&ptk=youtube_single&oid=0wI2ebPSPqHlphMwHIviqQ&ptchn=hhaM6li_V1-jecfiWKf6mA&pltype=content",
+      color: ["#F0BB73", "#2E2213"],
+      id: uuidv4(),
+      active: false,
+    },
+    //ADD MORE HERE
+  ];
+}
+
+export default music;
