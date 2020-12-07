02e03c55d51a6c735f3f150036b7e709bf63c00a
jaren <jarensmart@yahoo.com>
2020-12-06 23:30:58 -0500

    Added some more songs & styled Library UI

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ import "./styles/app.scss";
 //Components
 import Player from "./components/Player";
 import Song from "./components/Song";
+import Library from "./components/Library"
 //Util
 import data from './util';
 
 function App() {
   //State
   const [songs, setSongs] = useState(data());
-  const [currentSong, setCurrentSong] = useState(songs[6]);
+  const [currentSong, setCurrentSong] = useState(songs[0]);
   const [isPlaying, setIsPlaying] = useState(false);
 
   return (
     <div className="App">
       <Song currentSong={currentSong} />
       <Player isPlaying={isPlaying} setIsPlaying={setIsPlaying} currentSong={currentSong} />
+      <Library songs={songs} />
     </div>
   );
 }
